ef1107145f817c213dbc933548cc53ca642fa582
coltonmilbrandt <coltonmilbrandt@gmail.com>
2022-07-19 19:00:02 -0600

    updated staking logic

--- components/Stake.js
+++ components/Stake.js
@@ -7,23 +7,32 @@ const TOKEN_FARM_CONTRACT_ADDRESS = "0xCB157CA76f07F61988FfaFF272eb3BbAA8B94Bd6"
 
 
 export default function Stake() {
+
     const { isWeb3Enabled } = useMoralis()
     const [userTotalValue, setUserTotalValue] = useState("0")
+    const [tokenValue, setTokenValue] = useState("0")
     // Stake to contract
     const { runContractFunction: stakeTokens } = useWeb3Contract({
         abi: abi,
-        contractAddress: TOKEN_FARM_CONTRACT_ADDRESS,
+        contractAddress: "0xCB157CA76f07F61988FfaFF272eb3BbAA8B94Bd6",
         functionName: "stakeTokens",
-        msgValue: "10000",
         params: {},
+        msgValue: "10000000000000000",
     })
 
     // View Functions
     const { runContractFunction: getUserTotalValue} = useWeb3Contract({
         abi: abi,
-        contractAddress: TOKEN_FARM_CONTRACT_ADDRESS,
+        contractAddress: "0xCB157CA76f07F61988FfaFF272eb3BbAA8B94Bd6",
         functionName: "getUserTotalValue",
         params: {},
+    })
+
+    const { runContractFunction: getTokenValue} = useWeb3Contract({
+        abi: abi,
+        contractAddress: TOKEN_FARM_CONTRACT_ADDRESS,
+        functionName: "getTokenValue",
+        params: {address: 0xb01B218f021E9151c61eCEA3173361BbbE9eA346},
     })
 
     // This means that any time, any variable in here changes, run this function
@@ -32,6 +41,8 @@ export default function Stake() {
             async function updateUI() {
                 const userTotalValueFromCall = await getUserTotalValue()
                 setUserTotalValue(userTotalValueFromCall)
+                const tokenValueFromCall = await getTokenValue()
+                setTokenValue(tokenValueFromCall)
             }
             if (isWeb3Enabled) {
                 updateUI()
@@ -45,6 +56,7 @@ export default function Stake() {
             <div>
                 <h4>
                     You have <b>{userTotalValue ? userTotalValue : '0'}</b> tokens staked
+                    You have {tokenValue} token value
                 </h4>
             </div>
             <button 
