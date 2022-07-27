7769a3718bbda994657dc24fd61d59ad2fe4978e
coltonmilbrandt <coltonmilbrandt@gmail.com>
2022-07-26 18:53:08 -0600

    updated hooks, started parameterization

--- components/Stake.js
+++ components/Stake.js
@@ -1,36 +1,50 @@
 import { useState, useEffect } from "react"
 import { useMoralis, useWeb3Contract } from "react-moralis"
+import React from "react"
 // BE SURE to put "{ }" around abi
 import { abi } from "../constants/TokenFarm_abi"
 import { MockDaiAbi } from "../constants/MockDai_abi"
 import { ethers, utils } from "ethers"
+import contractsJson from "../constants/contractInfo.json"
 
-const TOKEN_FARM_CONTRACT_ADDRESS = "0x48efFEBe8879A7024654dda2d90F1EF56b12f135"
-let CURRENT_TOKEN_ADDRESS = "0x7F27377d72ba8c92f3fB3E1880Efc5583bD7f8Ab"
+// I added ESLint Plugin to check react hooks
 
+// TO DO
+// - Create a helpful script to retrieve current chain
+// - Make all functions modulare=
 
 
 export default function Stake() {
-
+    console.log({contractsJson})
     const { isWeb3Enabled, user } = useMoralis()
     const [userTotalValue, setUserTotalValue] = useState()
     const [tokenValue, setTokenValue] = useState([])
-    const approvedTokens = {}
+    const [contractOptions, setContractOptions] = useState({})
+    const [contractAbi, setContractAbi] = useState()
+    const [contractAddress, setContractAddress] = useState()
+    const [contractfunctionName, setcontractFunctionName] = useState()
+    const [contractParameters, setContractParameters] = useState({})
+    const contractsInfo = contractsJson[0]
+    console.log("below is the hopefully shorterarray")
+    console.log(contractsInfo)
+    console.log(contractsInfo.dependencies[0])
+
     // Stake to contract
     // WE NEED
     // 1. Approve the token
         // address
         // abi
         // chainId
-    const { runContractFunction: approveErc20 } = useWeb3Contract({
-        abi: MockDaiAbi,
-        contractAddress: "0xe1F284B9FB056cbF75A92c9b879594d1C74Fa7b9",
-        functionName: "approve",
-        params: {
-            spender: "0x48efFEBe8879A7024654dda2d90F1EF56b12f135",
-            amount: "1000000000000000000",
-        },
-    })
+    const { runContractFunction: approveErc20 } = useWeb3Contract(contractOptions)
+    // const { runContractFunction: approveErc20 } = useWeb3Contract({
+    //     abi: MockDaiAbi,
+    //     contractAddress: "0xe1F284B9FB056cbF75A92c9b879594d1C74Fa7b9",
+    //     functionName: "approve",
+    //     params: {
+    //         spender: "0x48efFEBe8879A7024654dda2d90F1EF56b12f135",
+    //         amount: "1000000000000000000",
+    //     },
+    // })
     // 2. Call the Staking function
         // address
         // abi
@@ -69,24 +83,8 @@ export default function Stake() {
         params: {_token: "0xe1F284B9FB056cbF75A92c9b879594d1C74Fa7b9"},
     })
 
-    // const getTokenPriceFeed = async (address) => {
-    //     try {
-    //         console.log("Running getTokenPriceFeed()...")
-    //         CURRENT_TOKEN_ADDRESS = address
-    //         var price
-    //         var decimals
-    //         const tokenValueFromCall = await getTokenValue(
-    //             price,
-    //             decimals,
-    //         )
-    //         price = tokenValueFromCall[0].toString()
-    //         decimals = tokenValueFromCall[1].toString()
-    //         CURRENT_TOKEN_ADDRESS = ""
-    //         return price, decimals
-    //     } catch (err) {
-    //         console.error(err)
-    //     }
-    // }
+    
+
 
     // This means that any time, any variable in here changes, run this function
     useEffect(() => {
@@ -129,7 +127,6 @@ export default function Stake() {
     }, [isWeb3Enabled])
 
     return (
-        
         <div>
             <div className="grid grid-cols-3 text-gray-900 pb-4">
                 <div className="bg-[#4E5166] p-4 rounded-md text-gray-100">
@@ -144,18 +141,32 @@ export default function Stake() {
                     </>
                 </h4>
             </div>
-            <button 
-                className="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                onClick={async () => {
-                    await approveErc20()
-                }}
-            >Approve Token</button>
-            <button 
-                className="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                onClick={async () => {
-                    await stakeTokens()
-                }}
-            >Stake Token</button>
+            <>
+                <button 
+                    className="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    onClick={async () => {
+                        setContractOptions({
+                            abi: MockDaiAbi,
+                            contractAddress: "0xe1F284B9FB056cbF75A92c9b879594d1C74Fa7b9",
+                            functionName: "approve",
+                            params: {
+                                spender: "0x48efFEBe8879A7024654dda2d90F1EF56b12f135",
+                                amount: "1000000000000000000",
+                            },
+                        })
+                        await approveErc20()
+                    }}
+                >Approve Token</button>
+            </>
+            
+            <>
+                <button 
+                    className="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    onClick={async () => {
+                        await stakeTokens()
+                    }}
+                >Stake Token</button>
+            </>
         </div>
     )
 }
