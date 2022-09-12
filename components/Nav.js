2d83691b61eeae20d2f2d78817d08a36af0b86c0
coltonmilbrandt <coltonmilbrandt@gmail.com>
2022-09-12 01:34:47 -0600

    styling fixes

--- components/Nav.js
+++ components/Nav.js
@@ -69,11 +69,11 @@ export default function Example() {
 							<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 								<div className="flex items-center justify-between h-16">
 									<div className="flex items-center">
-										<div className="flex-shrink-0">
+										<div className="flex flex-shrink-0 justify-center items-center">
 											<Image
 												className="h-8 w-8 rounded-full"
-												width="50%"
-												height="50%"
+												width="45%"
+												height="45%"
 												src="/../public/gwin-rect.webp"
 												alt="gwin"
 											/>
