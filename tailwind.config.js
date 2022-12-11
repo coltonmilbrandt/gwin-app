73348c3b1f543e9b301464dad9a5b2dd940d75ed
coltonmilbrandt <coltonmilbrandt@gmail.com>
2022-12-11 13:25:57 -0700

    fixed previous path fix that was wrong

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
 	content: [
-		"../pages/**/*.{js,ts,jsx,tsx}",
-		"../components/**/*.{js,ts,jsx,tsx}",
-		"../src/**/*.{html,js}",
-		"../node_modules/tw-elements/dist/js/**/*.js",
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+		"./src/**/*.{html,js}",
+		"./node_modules/tw-elements/dist/js/**/*.js",
 	],
 	theme: {
 		extend: {},
