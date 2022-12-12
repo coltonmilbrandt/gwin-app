d8d5a27e82177ac67296bb3fa9a04e31ee4b7901
coltonmilbrandt <coltonmilbrandt@gmail.com>
2022-12-12 12:43:42 -0700

    config retry

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
-/** @type {import('next').NextConfig} */
-
-const withOptimizedImages = require("next-optimized-images")
-
-module.exports = withOptimizedImages({
-	// your other Next.js configuration options
+// next.config.js
+const withPlugins = require("next-compose-plugins")
+const optimizedImages = require("next-optimized-images")
+const otherConfigSettings = {
 	reactStrictMode: true,
 	swcMinify: true,
 	eslint: {
@@ -12,4 +10,41 @@ module.exports = withOptimizedImages({
 	resolve: {
 		extensions: ["", ".js", ".jsx"],
 	},
-})
+}
+
+module.exports = withPlugins([
+	[
+		optimizedImages,
+		{
+			// these are the default values so you don't have to provide them if they are good enough for your use-case.
+			// but you can overwrite them here with any valid value you want.
+			inlineImageLimit: 8192,
+			imagesFolder: "images",
+			imagesName: "[name]-[hash].[ext]",
+			handleImages: ["jpeg", "png", "svg", "webp", "gif"],
+			removeOriginalExtension: false,
+			optimizeImages: true,
+			optimizeImagesInDev: false,
+			mozjpeg: {
+				quality: 80,
+			},
+			optipng: {
+				optimizationLevel: 3,
+			},
+			pngquant: false,
+			gifsicle: {
+				interlaced: true,
+				optimizationLevel: 3,
+			},
+			svgo: {
+				// enable/disable svgo plugins here
+			},
+			webp: {
+				preset: "default",
+				quality: 75,
+			},
+		},
+	],
+	// your other Next.js configuration options
+	otherConfigSettings,
+])
