4f4d5f4deb3557206102e4227dc3d15d07180d9a
coltonmilbrandt <coltonmilbrandt@gmail.com>
2023-01-05 17:54:38 -0700

    ReadMe updates

--- components/Welcome.js
+++ components/Welcome.js
@@ -4,7 +4,7 @@ import { ConnectButton } from "web3uikit"
 
 const Welcome = () => {
 	return (
-		<div className="bg-sky-50 m-3 max-w-3xl mx-auto shadow-lg px-6 py-10 rounded-lg text-gray-700">
+		<div className="bg-sky-50 m-3 max-w-2xl mx-auto shadow-lg px-6 py-10 rounded-lg text-gray-700">
 			<div className="flex justify-center w-full pb-3">
 				<Image
 					className="w-max h-max rounded-full "
@@ -19,15 +19,15 @@ const Welcome = () => {
 					<p className="mt-2 text-3xl leading-8 font-bold tracking-tight text-gray-900 sm:text-4xl sm:tracking-tight">
 						Welcome to Gwin!
 					</p>
-					<p className="mt-4 max-w-2xl text-xl text-gray-500 mx-auto">
+					<p className="mt-4 max-w-2xl text-lg text-gray-500 mx-auto">
 						With Gwin, it's simple to create and trade an endless
-						variety of markets. Select the Goerli testnet and
-						connect your wallet to get started.
+						variety of markets. Select the Goerli testnet and&nbsp;
+						<b>connect your wallet</b> to get started.
 					</p>
 					<div className="w-full flex justify-center mt-6">
 						<button>
 							<a
-								className="text-[#2f7daf] font-semibold py-2.5 px-4 rounded-2xl border-2 border-sky-50 hover:border-2 hover:bg-gray-100 hover:border-[#a1bed9]"
+								className="text-[#3982b0] font-semibold py-2.5 px-4 rounded-2xl border-2 border-sky-50 hover:border-2 hover:bg-gray-50 hover:border-[#bbcfe2]"
 								href="https://coltonmilbrandt.gitbook.io/gwin/"
 								target="_blank"
 								rel="noopener"
