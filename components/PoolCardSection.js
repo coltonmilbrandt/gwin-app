c36470a6e9ba7e84294c22ace2caa1f4f60d37fe
coltonmilbrandt <coltonmilbrandt@gmail.com>
2023-01-17 11:46:17 -0700

    removed add pool button until only owner issue fixed

--- components/PoolCardSection.js
+++ components/PoolCardSection.js
@@ -33,7 +33,7 @@ const PoolCardSection = ({
 					{sectionName}
 				</h2>
 				<div className="col-span-1 relative flex justify-end">
-					{/* button to add a pool */}
+					{/* button to add a pool
 					<a
 						className="inline-block text-xl fill-white font-bold text-white hover:text-sky-800 hover:fill-sky-800 active:text-sky-900 active:fill-sky-900 cursor-pointer"
 						onClick={() => setIsCreatePoolOpen(true)}
@@ -46,7 +46,7 @@ const PoolCardSection = ({
 						>
 							<path d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512zM232 344V280H168c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H280v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z" />
 						</svg>
-					</a>
+					</a> */}
 				</div>
 			</div>
 			{/* list relevant pools */}
